b5ee62ab7a15e9fa5216b79920e128aac9575765
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-04-17 11:48:19 +0530

    Added middleware for error handling

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ mongoose.connect('mongodb://localhost:27017/SIH')
         console.log("OH NO ERROR");
     })
 
+const User = require("./models/userSchema");
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("index");
+})
+
+app.use((err, req, res, next) => {
+    if (err) {
+        res.send("error");
+        console.log(err);
+    }
 })
 
 app.listen(3000, () => {
