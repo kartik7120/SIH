66a41ec44ca9902dcd156b1230f48bfb73a2ce95
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-04-17 11:16:45 +0530

    added views folder

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");
